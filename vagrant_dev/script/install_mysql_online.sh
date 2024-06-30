a684fa79fbd910975b221b3d5dbfd282bf1311d5
MiMiKnight <victor2015yhm@gmail.com>
2024-07-01 01:46:19 +0800

    [Requirement Number]US202308182020 [Commit Description]配置优化

--- vagrant_dev/script/install_mysql_online.sh
+++ vagrant_dev/script/install_mysql_online.sh
@@ -11,67 +11,138 @@ error_exit (){
 # 切换root用户
 sudo su - root
 # 创建基本目录
-sudo mkdir -p /opt/backup /opt/app
+sudo mkdir -p \
+ /opt/backup \
+ /opt/app/mysql \
+ /opt/workspace/mysql/data
+# 创建mysql用户
+sudo groupadd -g 500 -o -r mysql
+sudo useradd -M -N -g mysql -o -r -d /home/mysql -s /usr/sbin/nologin -c "MySQL Server" -u 500 mysql
+# 查看是否新增用户成功
+awk '/mysql/{print $0}' /etc/passwd
+# 修改目录用户属性
+chown -R mysql:mysql /opt/app/mysql /opt/workspace/mysql
+chmod -R 750 /opt/app/mysql /opt/workspace/mysql
+### 查看GLIBC版本
+ldd --version
+# 下载MySQL二进制安装包
 sudo wget --timeout=600 --tries=2 \
- -O /opt/backup/mysql-8.0.29-linux-glibc2.12-x86_64.tar \
- https://mirrors.huaweicloud.com/mysql/Downloads/MySQL-8.0/mysql-8.0.29-linux-glibc2.12-x86_64.tar
-# 下载md5校验文件
-sudo wget --timeout=600 --tries=2 \
- -O /opt/backup/mysql-8.0.29-linux-glibc2.12-x86_64.tar.md5 \
- https://mirrors.huaweicloud.com/mysql/Downloads/MySQL-8.0/mysql-8.0.29-linux-glibc2.12-x86_64.tar.md5
+ -O /opt/backup/mysql-8.0.37-linux-glibc2.28-x86_64.tar \
+ https://downloads.mysql.com/archives/get/p/23/file/mysql-8.0.37-linux-glibc2.28-x86_64.tar
  # 安装 libaio library
 sudo apt-cache search libaio
 sudo apt-get install -y libaio-dev
 sudo apt-get install -y libaio1t64
+sudo apt-get install -y libncurses6
 sudo wget --timeout=600 --tries=2 \
  -O /opt/backup/libaio1_0.3.113-5_amd64.deb \
  http://archive.ubuntu.com/ubuntu/pool/main/liba/libaio/libaio1_0.3.113-5_amd64.deb
 sudo dpkg -i /opt/backup/libaio1_0.3.113-5_amd64.deb
-# 校验
-sudo md5sum -c mysql-8.0.29-linux-glibc2.12-x86_64.tar.md5
-# 解压 mysql-8.0.29-linux-glibc2.12-x86_64.tar
-sudo mkdir -p /opt/backup/mysql8.0.29
-sudo tar xf /opt/backup/mysql-8.0.29-linux-glibc2.12-x86_64.tar \
-  --directory /opt/backup/mysql8.0.29
-# 解压安装 mysql-8.0.29-linux-glibc2.12-x86_64.tar.xz
-sudo tar xf /opt/backup/mysql8.0.29/mysql-8.0.29-linux-glibc2.12-x86_64.tar.xz \
-  --directory /opt/app
-sudo ln -s /opt/app/mysql-8.0.29-linux-glibc2.12-x86_64 /usr/local/mysql
+# 解压 mysql tar 压缩包
+sudo mkdir -p /opt/backup/mysql-8.0.37
+sudo tar xf /opt/backup/mysql-8.0.37-linux-glibc2.28-x86_64.tar \
+  --directory /opt/backup/mysql-8.0.37
+# 解压安装 mysql tar.xz 压缩包
+sudo tar xf /opt/backup/mysql-8.0.37/mysql-8.0.37-linux-glibc2.28-x86_64.tar.xz \
+  --directory /opt/app/mysql
+# 修改目录用户属性
+chown -R mysql:mysql /opt/app/mysql
+chown -R mysql:mysql /opt/workspace/mysql
+# 建立软链接
+sudo ln -s /opt/app/mysql/mysql-8.0.37-linux-glibc2.28-x86_64 /usr/local/mysql
+chown -R mysql:mysql /usr/local/mysql
+ln -s /opt/workspace/mysql/data /usr/local/mysql/data
+chown -R mysql:mysql /usr/local/mysql/data
 # 设置MySQL环境变量
-sudo export PATH=/usr/local/mysql/bin:$PATH
-# 创建mysql用户
-sudo groupadd -g 500 -o -r mysql
-sudo useradd -M -N -g mysql -o -r -d /home/mysql -s /usr/sbin/nologin -c "MySQL Server" -u 500 mysql
-# 为导入和导出操作创建安全目录
-sudo mkdir -p /usr/local/mysql/mysql-files
-sudo chown -R mysql:mysql /usr/local/mysql/mysql-files
-sudo chmod 750 -R /usr/local/mysql/mysql-files
+sudo cat > /etc/profile.d/mysql.sh << EOF
+export MYSQL_HOME=/usr/local/mysql
+export PATH=\$MYSQL_HOME/bin:\$MYSQL_HOME/support-files:\$PATH
+EOF
+# 环境变量生效
+source /etc/profile
+# 验证
+mysql -V
 # 创建MySQL配置文件
-sudo cat > /usr/local/mysql/my.cnf << EOF
+sudo cat > /opt/workspace/mysql/my.cnf << EOF
+############################[client]###############################
+[client]
+# 客户端连接默认端口号
+port=3306
+# 客户端连接时字符集默认编码
+default-character-set=utf8mb4
+# 指定socket
+#socket=/usr/local/mysql/mysql.sock
+############################[mysql]###############################
 [mysql]
+# 开启命令自动补齐给你
+no-auto-rehash
+# 字符集默认编码
 default-character-set=utf8mb4
 [client]
 #port=3306
+############################[mysqld]###############################
 [mysqld]
-user=mysql
-port=3306
-#server-id=100
+# 数据库唯一标识,主从必用且唯一
+server-id=100
+# 安装目录
 basedir=/usr/local/mysql
+# 数据位置
 datadir=/usr/local/mysql/data
-socket=/usr/local/mysql/mysql.sock
-log-error=/usr/local/mysql/data/mysql.err
-secure_file_priv=/usr/local/mysql/mysql-files
-local_infile=OFF
+# 指定socket
+#socket=/usr/local/mysql/mysql.sock
+# 指定PID文件
+#pid-file=/usr/local/mysql/mysqld.pid
+# 指定缓存目录
+#tmpdir=
+# 以什么用户运行
+user=mysql
+# 服务端端口号
+port=3306
+# 允许连接数据库的主机地址
+bind_address=0.0.0.0
+# 服务端字符集默认编码
+character-set-server=utf8mb4
+# 数据库默认时区
+default-time-zone='+00:00'
+# 默认存储引擎类型
+default-storage-engine=INNODB
+# 开启自动提交
 autocommit=1
+# 错误日志
+log-error=/usr/local/mysql/data/mysql.err
+# 开启慢查询日志
+slow-query-log=on
+# 慢查询日志路径
+#slow-query-log-file=
+# 慢查询的执行用时上限,默认设置是10s,推荐(1s~2s)
+long-query-time=2
+# 记录没有使用索引查询语句
+log-queries-not-using-indexes=0
+##########################[mysqldump]##############################
+[mysqldump]
+#设定在网络传输中一次消息传输量的最大值。系统默认值为1MB，最大值是1GB，必须设置为1024的倍数。单位为字节。
+max-allowed-packet=2M
+##########################[mysqld_safe]############################
+[mysqld_safe]
+open-files-limit=8192
 EOF
+# 创建配置文件软连接
+ln -s /opt/workspace/mysql/my.cnf /usr/local/mysql/my.cnf
 
-sudo chown -R root:root /usr/local/mysql/my.cnf
-sudo chmod 644 -R /usr/local/mysql/my.cnf
-# 初始化数据文件夹
-sudo mkdir -p /usr/local/mysql/data
-sudo chown -R mysql:mysql /usr/local/mysql/data
-sudo chmod 750 -R /usr/local/mysql/data
-sudo /usr/local/mysql/bin/mysqld --defaults-file=/usr/local/mysql/my.cnf --initialize
+# 初始化数据（默认root密码为空）
+sudo /usr/local/mysql/bin/mysqld \
+ --user=mysql \
+ --basedir=/usr/local/mysql \
+ --datadir=/usr/local/mysql/data \
+ --initialize-insecure
+# 修改目录或者文件权限
+chown -R mysql:mysql /opt/app/mysql
+chown -R mysql:mysql /opt/workspace/mysql
+chown root:root /opt/workspace/mysql/my.cnf
+chmod 644 /opt/workspace/mysql/my.cnf
+# 不允许删除my.cnf
+chattr +i /opt/workspace/mysql/my.cnf
+lsattr /opt/workspace/mysql/my.cnf
 # 创建自启动文件
 sudo cat > /usr/lib/systemd/system/mysqld.service << EOF
 [Unit]
@@ -112,22 +183,31 @@ Environment=MYSQLD_PARENT_PID=1
 
 PrivateTmp=false
 EOF
-
+# 设置自启动配置文件权限
 sudo chmod 644 /usr/lib/systemd/system/mysqld.service
 # 开启开机自启动
-systemctl enable mysqld.service
+sudo systemctl enable mysqld.service
 # 启动MySQL
-systemctl start mysqld
+sudo systemctl start mysqld
 # 查看MySQL状态
-systemctl status mysqld
+sudo systemctl status mysqld
 # 重置MySQL密码
 sudo /usr/bin/expect << EOF
 set timeout 60
 spawn /usr/local/mysql/bin/mysql -u root -p
-expect "Enter password:" { send "p\r" }
+expect "Enter password:" { send "\r" }
+expect "mysql>" { send "alter user 'root'@'localhost' identified by '123456';\r" }
+expect "mysql>" { send "flush privileges;\r" }
+expect "mysql>" { send "quit;\r" }
 expect eof
 EOF
+# 重启MySQL
+sudo systemctl restart mysqld
+# 查看MySQL状态
+sudo systemctl status mysqld
 # 切换vagrant用户
 sudo su - vagrant
+# 清理
+sudo apt-get autoclean
 #
 sudo echo "Install MySQL success!!!"
