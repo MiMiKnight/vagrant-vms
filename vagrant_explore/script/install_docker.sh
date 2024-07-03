ebc20d4e145b07a0e4b62648d848fd7241c70445
MiMiKnight <victor2015yhm@gmail.com>
2024-07-03 23:34:32 +0800

    [Requirement Number]US202308182020 [Commit Description]优化配置

---
+++ vagrant_explore/script/install_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# 显示指令及参数
+sudo set -ex
+# 安装Docker
+#
+sudo echo "Install docker success!!!"
---
+++ vagrant_explore/script/vm_mysql.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# 显示指令及参数
+sudo set -ex
+sudo hostnamectl hostname mysql.explore.vm.mimiknight.cn
+sudo echo "Install MySQL VM finished !!!"
