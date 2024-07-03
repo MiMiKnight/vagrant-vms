8e06bb810b82b5176c1923c372a4d4fea63cd3da
MiMiKnight <victor2015yhm@gmail.com>
2024-07-04 00:20:37 +0800

    [Requirement Number]US202308182020 [Commit Description]配置优化

---
+++ vagrant_explore/script/install_redis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 官网教程链接 https://redis.io/docs/latest/operate/oss_and_stack/install/install-redis/install-redis-on-linux/
+# 显示指令及参数
+sudo set -ex
+# 安装Redis
+#
+sudo apt install lsb-release curl gpg
+#
+sudo echo "Install docker success!!!"
---
+++ vagrant_explore/script/vm_redis.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# 显示指令及参数
+sudo set -ex
+sudo hostnamectl hostname redis.explore.vm.mimiknight.cn
+sudo echo "Install redis VM finished !!!"
