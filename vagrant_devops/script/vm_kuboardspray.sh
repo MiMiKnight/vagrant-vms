f29bd4dfd69b7a37fd87501787d20d46364237b3
MiMiKnight <victor2015yhm@gmail.com>
2024-07-04 22:39:23 +0800

    [Requirement Number]US202308182020 [Commit Description]优化

--- vagrant_devops/script/vm_kuboardspray.sh
+++ vagrant_devops/script/vm_kuboardspray.sh
@@ -7,7 +7,7 @@ sudo set -ex
 sudo mkdir -p \
  /opt/workspace/data/kuboard-spray
 #
-docker pull swr.cn-east-2.myhuaweicloud.com/kuboard/kuboard-spray:v1.2.4-amd64
+sudo docker pull swr.cn-east-2.myhuaweicloud.com/kuboard/kuboard-spray:v1.2.4-amd64
 #
 sudo docker run -d \
   --privileged \
