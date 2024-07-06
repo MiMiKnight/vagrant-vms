52a54e5b45a5ca88a3b4043c5b6280474f8eca2c
MiMiKnight <victor2015yhm@gmail.com>
2024-07-06 15:17:03 +0800

    [Requirement Number]US202308182020 [Commit Description]优化

--- vagrant_dev_k8s/script/vm_public.sh
+++ vagrant_dev_k8s/script/vm_public.sh
@@ -9,7 +9,7 @@ set -ex
 # 安装系统基础软件
 sudo apt-get install -y \
   dialog vim iputils-ping lrzsz tcl expect \
-  ntpdate gdisk parted telnet axel
+  ntpdate gdisk parted telnet axel dos2unix
 # 设置NTP服务器
 sudo ntpdate ntp.vm.mimiknight.cn
 #sudo ntpdate ntp.aliyun.com
