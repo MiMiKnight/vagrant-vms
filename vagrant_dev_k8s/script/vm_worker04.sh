c1a31fb53784ddee5775cf3cf11b4c00b7bd7edd
MiMiKnight <victor2015yhm@gmail.com>
2024-07-06 17:19:25 +0800

    [Requirement Number]US202308182020 [Commit Description]优化

---
+++ vagrant_dev_k8s/script/vm_worker04.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# 显示指令及参数
+set -ex
+#
+sudo hostnamectl hostname worker04.k8s.dev.vm.mimiknight.cn
+sudo echo "Install K8S Worker04 VM finished !!!"
---
+++ vagrant_dev_k8s/script/vm_worker05.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# 显示指令及参数
+set -ex
+#
+sudo hostnamectl hostname worker05.k8s.dev.vm.mimiknight.cn
+sudo echo "Install K8S Worker05 VM finished !!!"
